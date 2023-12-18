7b5df5d352e0c76a0bae3904fdaae36dae5a73d9
Michajlo <vedmidmetal@ukr.net>
2023-12-18 14:57:38 +0100

    initial

---
+++ backend/src/main/java/de/ukrainewin/helpukraine/controller/FoodController.java
@@ -0,0 +1,48 @@
+package de.ukrainewin.helpukraine.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import de.ukrainewin.helpukraine.model.Food;
+import de.ukrainewin.helpukraine.model.FoodDTO;
+import de.ukrainewin.helpukraine.service.FoodService;
+
+@RestController
+@RequestMapping("api/food")
+public class FoodController {
+
+	@Autowired
+	FoodService foodService;
+
+	@GetMapping
+	public List<Food> listFoods(@RequestParam Optional<String> search) {
+		if (search.isPresent()) {
+			return foodService.search(search.get());
+		}
+		return foodService.list();
+	}
+
+	@GetMapping("{id}")
+	public Food getFood(@PathVariable String id) {
+		return foodService.findById(id);
+	}
+
+	@PostMapping()
+	public Food addFood(@RequestBody FoodDTO foodDTO) {
+		return foodService.addFood(foodDTO);
+	}
+
+	@DeleteMapping("{id}")
+	public void deleteFood(@PathVariable String id) {
+		foodService.delete(id);
+	}
+	
+	@PutMapping("{id}")
+	public void putFood(@PathVariable String id) {
+		foodService.delete(id);
+	}
+
+}
